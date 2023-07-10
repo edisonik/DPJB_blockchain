5bda37ae14c5b0976ea8bbec173f6b27f5934de0
Brunocm <brunocmarino@gmail.com>
2023-07-10 20:20:17 -0300

    Add ideias de quais dados incluir na blockchain

--- fabric-samples/asset-transfer-basic/chaincode-typescript/src/asset.ts
+++ fabric-samples/asset-transfer-basic/chaincode-typescript/src/asset.ts
@@ -4,6 +4,19 @@
 
 import {Object, Property} from 'fabric-contract-api';
 
+// Exemplo de dados para serem adicionados à BlockChain
+// 
+// Tipo : 'jornalista' | 'organização' | 'empresa de mídia' | 'funcionário' | 'Obra'
+// ID : string (identificador) 
+// Nome : string (50 caracteres)
+// Responsável : number (responsável por cadastrar)
+// Estado : 'Cadastro' | 'Atualização' | 'Exclusão' 
+// Link ?: string (100 caracteres?) (link do site ou do currículo)
+// Descrição ?: string (280 caracteres)
+// Organização Responsável ?: string (id da organização)
+// Empresa Responsável ?: string (id da organização) 
+
+
 @Object()
 export class Asset {
     @Property()
